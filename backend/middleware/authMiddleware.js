42184af0ad4e4c796608da670ba710ade566e28a
Chamal Senarathna <chamals004@gmail.com>
2026-04-07 14:50:44 +0530

    feat: auth

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Not authorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token invalid" });
+  }
+};
+
+export const adminOnly = (req, res, next) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({ message: "Admin access only" });
+  next();
+};
+
+export const wardenOrAdmin = (req, res, next) => {
+  if (!["admin", "warden"].includes(req.user.role))
+    return res.status(403).json({ message: "Access denied" });
+  next();
+};
